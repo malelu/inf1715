b682eaed13d868f53846f778d1b11d4c92f9b924
Marina <marina.lucena@globo.com>
2014-02-26 21:17:25 -0300

    regras completas

--- trab1/main.c
+++ trab1/main.c
@@ -52,17 +52,53 @@ int main (void)
 			case TK_FUN :
 				printf ("FUN\n");
 			break;
-			case TK_GE :
-				printf ("GE\n");
+			case TK_OPEN_PARENTHESIS :
+				printf ("OPEN_PARENTHESIS\n");
 			break;
-			case TK_LE :
-				printf ("LE\n");
+			case TK_CLOSE_PARENTHESIS :
+				printf ("CLOSE_PARENTHESIS\n");
 			break;
-			case TK_EE :
-				printf ("EE\n");
+			case TK_COMMA :
+				printf ("COMMA\n");
 			break;
-			case TK_NE :
-				printf ("NE\n");
+			case TK_COLLON :
+				printf ("COLLON\n");
+			break;
+			case TK_GREATER :
+				printf ("GREATER\n");
+			break;
+			case TK_LESS :
+				printf ("LESS\n");
+			break;
+			case TK_GREATER_EQUAL :
+				printf ("GREATER_EQUAL\n");
+			break;
+			case TK_LESS_EQUAL :
+				printf ("LESS_EQUAL\n");
+			break;
+			case TK_EQUAL :
+				printf ("EQUAL\n");
+			break;
+			case TK_NOT_EQUAL :
+				printf ("NOT_EQUAL\n");
+			break;
+			case TK_OPEN_BRACKET :
+				printf ("OPEN_BRACKET\n");
+			break;
+			case TK_CLOSE_BRACKET :
+				printf ("CLOSE_BRACKET\n");
+			break;
+			case TK_PLUS :
+				printf ("PLUS\n");
+			break;
+			case TK_MINUS :
+				printf ("MINUS\n");
+			break;
+			case TK_TIMES :
+				printf ("TIMES\n");
+			break;
+			case TK_DIVIDED :
+				printf ("DIVIDED\n");
 			break;
 			case TK_AND :
 				printf ("AND\n");
@@ -80,10 +116,10 @@ int main (void)
 				printf ("ID->%s\n", val.cValor);
 			break;
 			case TK_LITERAL_STRING :			
-				printf("STRING->%s\n", val.cValor);
+				printf("LITERAL_STRING->%s\n", val.cValor);
 			break;
-			case OTHER :
-				printf ("%s\n", val.cValor);
+			case ERROR :
+				printf ("ERROR->%s\n", val.cValor);
 			break;
 
 		}
