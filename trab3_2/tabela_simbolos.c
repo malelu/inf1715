05316f93ca2a9d45e9742a610c6c9af8f5ef098f
Marina <marina.lucena@globo.com>
2014-04-27 11:25:05 -0300

    criando funcoes da tabela de simbolos

--- trab3_2/tabela_simbolos.c
+++ trab3_2/tabela_simbolos.c
@@ -2,8 +2,6 @@
 
 
 
-
-
 Symbol* Symbol_new(char* name, SymbolType type, int line)
 {
    	Symbol* sym = (Symbol*) malloc(sizeof(Symbol)) ;
@@ -13,6 +11,17 @@ Symbol* Symbol_new(char* name, SymbolType type, int line)
 	sym->line = line ;
 
 	return sym ;
+}
+
+NodeTable* NodeTable_new(Symbol* sym)
+{
+	NodeTable* node = (NodeTable*) malloc(sizeof(NodeTable)) ;
+	node->nextNode = NULL;
+   	node->prevNode = NULL;
+   	node->symbol = (Symbol*) malloc(sizeof(Symbol)) ;
+	node->Symbol = sym ;
+
+	return node ;
 }
 
 
@@ -25,10 +34,6 @@ SymbolTable* SymbolTable_new()
 	return table ;
 }
 
-NodeTable* NodeTable_new()
-{
-	NodeTable* table = (NodeTable*) malloc(sizeof(NodeTable)) ;
-}
 
 void SymbolTable_delete(SymbolTable* st)
 {
@@ -37,11 +42,45 @@ void SymbolTable_delete(SymbolTable* st)
 void SymbolTable_add(SymbolTable* st, const char* name, SymbolType type, int line)
 {
 
-	Symbol sym = Symbol_new(name, type, line) ;
-	st->lastSymbol
-	st->lastSymbol = sym ;
+	Symbol* sym = Symbol_new(name, type, line) ;
+	NodeTable* new_node = NodeTable_new(&sym) ;
+	NodeTable* old_last_node = NULL;
+	
+	if(st->firstNode == NULL)
+	{
+		st->firstNode = (NodeTable*) malloc(sizeof(NodeTable)) ;
+		st->lastNode = (NodeTable*) malloc(sizeof(NodeTable)) ;
+		st->firstNode = new_node ;
+		st->lastNode = new_node ; 
+	}
+	else
+	{
+		old_last_node = (NodeTable*) malloc(sizeof(NodeTable)) ;
+		new_node->prevNode = (NodeTable*) malloc(sizeof(NodeTable)) ;
+
+		old_last_node = st->lastNode ;
+		old_last_node->nextNode = new_node ;
+		new_node->prevNode = old_last_node ;
+		st->lastNode = new_node ;
+	}
 }
 
 Symbol* SymbolTable_get(SymbolTable* st, const char* name)
 {
+
+	NodeTable* node = (NodeTable*) malloc(sizeof(NodeTable)) ;
+	node = st->firstNode ;
+
+	while(strcmp(node->symbol->name, name) != 0)
+	{
+		if(node->nextNode == NULL)
+		{
+			fprintf(stderr, "no symbol match this name") ;
+			return NULL ;
+		}			
+		node = node->nextNode ;
+	}
+
+	return node->Symbol ;
+	
 }
